cc09c6ef5d6a13a556ca377c044d64ecb8e3ff10
Khang Le <tkhang1999@gmail.com>
2018-12-30 04:14:43 +1100

    Node HTTP Example 1

--- node-examples/index.js
+++ node-examples/index.js
@@ -3,13 +3,17 @@ const rect = require("./rectangle.js");
 const solveRect = (l, b) => {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
 
-    if (l <= 0 || b <= 0){
-        console.log("Rectangle dimensions should be greater than zero: l = " + l + ", and b = " + b);
-    }
-    else{
-        console.log("The area of the rectangle is " + rect.area(l,b));
-        console.log("The perimeter of the rectangle is " + rect.perimeter(l,b));
-    }
+    rect(l,b, (err,rectangle) => {
+        if (err) {
+            console.log("ERROR: " + err.message);
+        }
+        else{
+            console.log("The area of the rectangle: " + rectangle.area());
+            console.log("The perimeter of the rectangle: " + rectangle.perimeter());
+        }
+    });
+
+    console.log("This statement is after the call to rect()");
 }
 
 solveRect(2,4);
