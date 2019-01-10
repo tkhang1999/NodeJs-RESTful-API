170384aa3f03012c730503968d0a2fde347f602f
Khang Le <tkhang1999@gmail.com>
2019-01-10 11:44:11 +0700

    Passport

---
+++ conFusionServer/authenticate.js
@@ -0,0 +1,7 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('./models/user.js');
+
+exports.local = passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
